bb81b2c2745d88756fb927c6be8833a4819b0b33
iamshiv007 <Github@#007>
2023-11-12 13:12:40 +0530

    delete and update

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,54 @@ import { Task } from "./shared/Task";
 
 const taskRepo = remult.repo(Task);
 const tasks = ref<Task[]>([]);
-onMounted(() => taskRepo.find().then((items: any) => (tasks.value = items)));
+
+const newTaskTitle = ref("")
+async function addTask() {
+  try {
+    const newTask = await taskRepo.insert({ title: newTaskTitle.value })
+    tasks.value.push(newTask)
+    newTaskTitle.value = ""
+  } catch (error) {
+    alert((error as { message: string }).message)
+  }
+}
+
+async function saveTask(task: Task) {
+  try {
+    await taskRepo.save(task)
+  } catch (error) {
+    alert((error as { message: string }).message)
+  }
+}
+
+async function deleteTask(task: Task) {
+  try {
+    await taskRepo.delete(task)
+    tasks.value = tasks.value.filter(t => task !== t)
+  } catch (error) {
+    alert((error as { message: string }).message)
+  }
+}
+
+onMounted(() => taskRepo.find({
+  limit: 20,
+  // orderBy: { createdAt: "desc" },
+  // where: { completed: false }
+}).then((items: any) => (tasks.value = items)));
 </script>
 <template>
   <div>
     <h1>todos</h1>
     <main>
+      <form @submit.prevent="addTask()">
+        <input v-model="newTaskTitle" placeholder="What needs to be done?" />
+        <button>Add</button>
+      </form>
       <div v-for="task in tasks" :key="task.id">
-        <input type="checkbox" v-model="task.completed" />
-        {{ task.title }}
+        <input type="checkbox" v-model="task.completed" @change="saveTask(task)" />
+        <input v-model="task.title" />
+        <button @click="saveTask(task)">Save</button>
+        <button @click="deleteTask(task)">Delete</button>
       </div>
     </main>
   </div>
