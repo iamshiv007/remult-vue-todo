520992583db4db02da40080089327a8290402e36
iamshiv007 <Github@#007>
2023-11-12 11:36:16 +0530

    todo list showing

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { remult } from "remult";
+import { Task } from "./shared/Task";
+
+const taskRepo = remult.repo(Task);
+const tasks = ref<Task[]>([]);
+onMounted(() => taskRepo.find().then((items: any) => (tasks.value = items)));
+</script>
+<template>
+  <div>
+    <h1>todos</h1>
+    <main>
+      <div v-for="task in tasks" :key="task.id">
+        <input type="checkbox" v-model="task.completed" />
+        {{ task.title }}
+      </div>
+    </main>
+  </div>
+</template>
